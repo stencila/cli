ea247ba8a4cb717249f6c136d23964ac62272535
Nokome Bentley <nokome@stenci.la>
2017-05-22 17:29:50 +1200

    Add tests; make fixes and refinements

--- test.sh
+++ test.sh
@@ -1,12 +1,62 @@
 #!/usr/bin/env bash
 
-. sibyl
+. sibyl sourced
+
+# Testing functions
+
+passes=0
+fails=0
+
+function test_ {
+  printf "${magenta}TEST${normal} $1\n"
+}
+
+function pass {
+  printf "${green}PASS${normal} $1\n"
+  passes=$(($passes + 1))
+}
+
+function fail {
+  printf "${red}FAIL${normal} $1\n"
+  fails=$(($fails + 1))
+}
+
+function indent {
+  sed 's/^/    /'
+}
+
+# Assertion functions
+
+function assert_no_diff {
+  diff_output=$(diff $1 $2)
+  diff_status=$?
+  if [ "$diff_status" == 0 ]; then
+    pass $1
+  else
+    fail $1
+    echo "$diff_output" | indent
+  fi
+}
+
+# Iterate over test folders
 
 for dir in tests/*/
 do
-    echo "Running test ${dir}"
-    cd $dir
-    compile
-    cd ../..
-    echo
+  test_ $(basename $dir)
+  cd $dir
+  
+  compile
+  for file in $(ls -1 *.expected); do
+    assert_no_diff "${file%.*}" "$file"
+  done
+
+  cd ../..
+  
+  echo
 done
+
+# Overall stats
+
+info "Passed: ${green}$passes${normal} Failed: ${red}$fails${normal}"
+
+exit $fails
---
+++ tests/r-packages/r-requires.R
@@ -0,0 +1,4 @@
+options(repos=structure(c(CRAN='https://cran.rstudio.com')))
+install.packages('devtools')
+devtools::install_version("ggplot2", "1.0.1")
+devtools::install_version("dplyr", "0.4.1")
