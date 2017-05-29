1280736e9f97111f03ff191b54524aeb3d76093c
Nokome Bentley <nokome@stenci.la>
2017-05-29 18:42:29 +1200

    Various updates to Dockerfile and stencila-environ.sh incl adding Python

--- images/alpha/stencila-environ.sh
+++ images/alpha/stencila-environ.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-echo '{' \
-	'{"r":' "$(R --slave -e 'stencila:::environ()' 2> /dev/null || echo "null")" '}' \
-'}'
+echo -e '{\n' \
+	'"node":' "$(node -e "require('stencila-node').environ()" 2> /dev/null || echo "null")" ',\n' \
+	'"py":' "$(python -c "import stencila; stencila.environ()" 2> /dev/null || echo "null")" ',\n' \
+	'"r":' "$(R --slave -e "stencila:::environ()" 2> /dev/null || echo "null")" '\n' \
+'}\n' | jq .
