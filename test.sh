493c13aee5f96501b07c317fe5de06e74ac45b09
Nokome Bentley <nokome@stenci.la>
2017-05-31 12:19:07 +1200

    Use 'bundle' instead of 'folder'; change naming scheme

--- test.sh
+++ test.sh
@@ -15,6 +15,7 @@ passes=0
 fails=0
 
 function test_ {
+  echo
   echo -e "${magenta}TEST${normal} $1"
 }
 
@@ -30,6 +31,14 @@ function fail {
 
 # Assertion functions
 
+function assert_equal {
+  if [ "$1" == "$2" ]; then
+    pass "$1"
+  else
+    fail "$1 != $2"
+  fi
+}
+
 function assert_no_diff {
   diff_output=$(diff "$1" "$2")
   diff_status=$?
@@ -40,6 +49,13 @@ function assert_no_diff {
     echo "$diff_output" | indent
   fi
 }
+
+# Function tests
+
+test_ "name"
+assert_equal "$(name "file://folder/subfolder")" "file-folder-subfolder-4329642ba3"
+assert_equal "$(name "github://foo/bar/folder/subFOLDER")" "github-foo-bar-folder-subfolder-a022134984"
+assert_equal "$(name "file://home/user/Esperança-vôo-avião")" "file-home-user-esperanca-voo-aviao-2169b6d973"
 
 # Iterate over test folders
 
@@ -56,14 +72,12 @@ do
   # Compile
   compile
 
-  # Test files are as expected
+  # Test that files are as expected
   for file in *.expected; do
     assert_no_diff "${file%.*}" "$file"
   done
 
   cd ../..
-  
-  echo
 done
 
 # Overall stats
