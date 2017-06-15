0bc6622cb4e0b84ed3d68600ffa6c5649cb56a9a
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-06-15 18:47:12 +0200

    log events

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
+const http = require('http')
 const Koa = require('koa')
 const KoaRouter = require('koa-router')
+const logHttp = require('log-http')
 const send = require('koa-send')
 const PassThrough = require('stream').PassThrough
 const path = require('path')
+const pino = require('pino')
 const spawn = require('child_process').spawn
 
+const PORT = 3000
+
 const app = new Koa()
+const log = pino({ level: 'debug', name: 'sibyl' }, process.stdout)
 const router = new KoaRouter()
 
 // Static content in `client` folder
@@ -32,24 +38,31 @@ router.get('/~launch/*', ctx => {
   const address = ctx.path.substring(9)
   const mock = (typeof ctx.request.query.mock !== 'undefined') ? '--mock' : ''
   const sibyl = spawn('./sibyl.sh', ['launch', address, mock])
+
   sibyl.stdout.on('data', data => {
     for (let line of data.toString().split('\n')) {
       if (line.length) {
         const goto_ = line.match(/^GOTO (.+)$/)
         if (goto_) {
+          log.debug('SSE: sending stdout goto')
           sse.write(`event: goto\ndata: ${goto_[1]}\n\n`)
         } else {
+          log.debug('SSE: sending stdout data')
           sse.write(`event: stdout\ndata: ${line}\n\n`)
         }
       }
     }
   })
+
   sibyl.stderr.on('data', data => {
     for (let line of data.toString().split('\n')) {
+      log.debug('SSE: sending stderr data')
       sse.write(`event: stderr\ndata: ${line}\n\n`)
     }
   })
+
   sibyl.on('exit', code => {
+    log.debug('SSE: sending end event')
     sse.write(`event: end\ndata: ${code}\n\n`)
     ctx.res.end()
   })
@@ -65,7 +78,13 @@ router.get('/', async ctx => {
   await send(ctx, 'client/index.html')
 })
 
+// Start listening & attach http logger
 app.use(router.routes())
-
-app.listen(3000)
-console.log('Listening at http://127.0.0.1:3000')
+const server = http.createServer(app.callback())
+const stats = logHttp(server)
+stats.on('data', function (level, data) {
+  log[level](data)
+})
+server.listen(PORT, function () {
+  console.log('Listening at http://127.0.0.1:' + PORT)
+})
