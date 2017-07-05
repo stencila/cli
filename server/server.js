7bf0f0ab316403c5dad6e5bebc7577e1fe946fc7
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-07-05 11:24:55 +0200

    merge errors into server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,31 @@ const send = require('send')
 const url = require('url')
 
 const Sibyl = require('./sibyl')
-const errors = require('./errors')
+
+const errors = {
+  EPIPE: function (req, res, ctx, err) {
+    const url = req.url
+    err.message += ' for url ' + url
+    ctx.log.error(err)
+  },
+  EURLNOTFOUND: function (req, res, ctx) {
+    ctx.log.warn('path not found for', req.url)
+    ctx.send(404, { message: 'not found' })
+  },
+  ESESSIONINVALID: function (req, res, ctx, err) {
+    ctx.log.warn('Invalid session ID')
+    ctx.send(403, { message: 'Invalid session ID' })
+  },
+  EFORMPARSE: function (req, res, ctx, err) {
+    var msg = err.message
+    ctx.log.warn(msg)
+    ctx.send(403, { message: msg })
+  },
+  EBETATOKENINVALID: function (req, res, ctx, err) {
+    ctx.log.warn('Invalid token', req.url)
+    res.write(`event: stderr\ndata: Invalid beta token\n\n`)
+  }
+}
 
 const env = {
   PORT: 3000,            // Port for the server to listen on
