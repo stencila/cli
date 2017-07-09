c779cdb26c5d012c90ceb7e27577d848376bda33
Nokome Bentley <nokome@stenci.la>
2017-07-09 11:33:45 +0200

    Fix regex, add a test

--- tests/sibyl.js
+++ tests/sibyl.js
@@ -6,11 +6,23 @@ var fs = require('fs')
 
 var Sibyl = require('../lib/sibyl')
 
-tape('should fetch from disk', function (assert) {
+tape('should fetch from disk directory', function (assert) {
   assert.plan(2)
   var dir = path.join('/tmp', uuid().slice(0, 6))
   var sibyl = Sibyl({ directory: dir })
   var address = 'file://' + path.join(__dirname, 'fetch-file/hello')
+  sibyl.fetch(address, function (err, res) {
+    assert.ifError(err, 'no error fetching')
+    var files = fs.readdirSync(dir)
+    spok(assert, files, [ 'main.md' ])
+  })
+})
+
+tape('should fetch from disk tar archive', function (assert) {
+  assert.plan(2)
+  var dir = path.join('/tmp', uuid().slice(0, 6))
+  var sibyl = Sibyl({ directory: dir })
+  var address = 'file://' + path.join(__dirname, 'fetch-file/hello.tar.gz')
   sibyl.fetch(address, function (err, res) {
     assert.ifError(err, 'no error fetching')
     var files = fs.readdirSync(dir)
