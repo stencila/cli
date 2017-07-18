d26a59ddd3325b084671d6711e8c75e78b2ccb95
Nokome Bentley <me@noko.me>
2017-07-19 11:08:33 +1200

    Pull image if not present

--- tests/run.js
+++ tests/run.js
@@ -4,12 +4,12 @@ var run = require('../lib/run')
 
 tape('run should run a container', function (assert) {
   assert.plan(7)
-  run('stencila/iota', function (err, url, data, container) {
+  run('stencila/iota:latest', function (err, url, data, container) {
     assert.ifError(err, 'no err')
     assert.ok(url.match(/^http:\/\/localhost:\d+/))
     container.inspect(function (err, data) {
       assert.ifError(err, 'no err')
-      assert.equal(data.Config.Image, 'stencila/iota')
+      assert.equal(data.Config.Image, 'stencila/iota:latest')
       assert.equal(data.State.Status, 'running')
       container.stop(function (err, data) {
         assert.ifError(err, 'no err')
