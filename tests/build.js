24ddc62b8d1621dad867f081dd9270f0bef61ab3
Nokome Bentley <me@noko.me>
2017-07-18 17:41:22 +1200

    Building

--- tests/build.js
+++ tests/build.js
@@ -1,13 +1,30 @@
 var path = require('path')
 var tape = require('tape')
+var tmp = require('tmp')
 
+var fetch = require('../lib/fetch')
+var compile = require('../lib/compile')
 var build = require('../lib/build')
 
-tape('should build a thing', function (assert) {
-  assert.plan(1)
-  var name = 'foo'
-  var location = path.join(__dirname, 'fixtures/diamonds')
-  build(location, name, function (err, route) {
+// Creates a temp dir, fetches the test fixture, compiles
+// a Dockerfile and builds a container for it
+function buildImage (fixture, name, cb) {
+  tmp.dir(function (err, directory) {
+    if (err) return cb(err)
+    fetch('file', fixture, null, directory, function (err, res) {
+      if (err) return cb(err)
+      compile(directory, function (err) {
+        if (err) return cb(err)
+        build(directory, name, cb)
+      })
+    })
+  })
+}
+
+tape('build should build something', function (assert) {
+  buildImage(path.join(__dirname, 'fixtures', 'diamonds'), 'aardvark:alice', function (err, stream) {
     assert.ifError(err, 'no err')
+    stream.pipe(process.stdout)
+    assert.end()
   })
 })
