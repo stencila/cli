319129c7c3ce0636e6048dd498398b13eaf821f4
Nokome Bentley <me@noko.me>
2017-07-19 08:54:26 +1200

    Fixes lib/run

--- tests/run.js
+++ tests/run.js
@@ -2,17 +2,18 @@ var tape = require('tape')
 
 var run = require('../lib/run')
 
-tape.skip('run should run a container', function (assert) {
-  assert.plan(6)
-  run('stencila/alpha', function (err, res) {
+tape('run should run a container', function (assert) {
+  assert.plan(7)
+  run('stencila/alpha', function (err, url, data, container) {
     assert.ifError(err, 'no err')
-    res.container.inspect(function (err, data) {
+    assert.ok(url.match(/^http:\/\/localhost:\d+/))
+    container.inspect(function (err, data) {
       assert.ifError(err, 'no err')
       assert.equal(data.Config.Image, 'stencila/alpha')
       assert.equal(data.State.Status, 'running')
-      res.container.stop(function (err, data) {
+      container.stop(function (err, data) {
         assert.ifError(err, 'no err')
-        res.container.remove(function (err, data) {
+        container.remove(function (err, data) {
           assert.ifError(err, 'no err')
         })
       })
